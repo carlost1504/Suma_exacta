18148478d4f73772d544fc5aa1667b5680f7d1b2
carlost1504 <61698120+carlost1504@users.noreply.github.com>
2021-10-03 21:40:22 -0500

    t1

--- src/Ui/Main.java
+++ src/Ui/Main.java
@@ -1,6 +1,9 @@
 package Ui;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.Scanner;
 
@@ -15,7 +18,7 @@ public class Main {
 	}
 	
 	
-	int binarySearch(int arr[], int lo, int hi, int x) {
+	public int binarySearch(int arr[], int lo, int hi, int x) {
         if (hi >= lo && lo < arr.length - 1) {
             int mid = lo + (hi - lo) / 2;
             System.out.println("Entre");
@@ -30,11 +33,30 @@ public class Main {
         return -1;
     }
 	
+	public int BinarySearch1(int arr[], int x) {
+		   int lo = 0, hi = arr.length - 1;
+		   while (lo <= hi) {
+			   int mid = lo + (hi - lo) / 2;
+
+		       if (arr[mid] == x)
+		    	   return mid;
+
+		       if (arr[mid] < x)
+		           lo = mid + 1;
+
+		       else
+		           hi = mid - 1;
+		       }
+		       return -1;
+		}
+	
 	public void createArray() {
 		int arr[];
 		int longg = 0;
 		ArrayList<String> Exit = new ArrayList<String>();
 		ArrayList<String> Exitt = new ArrayList<String>();
+		ArrayList<int[]> sum = new ArrayList<int[]>();
+		ArrayList<int[]> sum1 = new ArrayList<int[]>();
 		longg= Integer.parseInt(sc.nextLine());
 		arr=new int[longg];
 		
@@ -55,6 +77,8 @@ public class Main {
 		    	Exit.add(n1);
 		    }
 		    else {
+		    	int[] suma= {arr[i],arr[position]};
+		    	sum.add(suma);
 		        String n2=("Element found at index: " + arr[position] + "valor busqueda "+ arr[i] );
 		        Exit.add(n2);
 		    }
@@ -64,32 +88,61 @@ public class Main {
 		    	Exitt.add(n1);
 		    }
 		    else {
+		    	int[] suma= {arr[i],arr[position]};
+		    	sum1.add(suma);
 		        String n2=("Element found at index: " + arr[position] + "valor busqueda "+ arr[i] );
 		        Exitt.add(n2);
 		    }
 	    }
+	    
+	    int[] sumados=busqueda( sum);
+	    System.out.println(Arrays.toString(sumados));
+	    int[] sumados1=busqueda( sum1);
 	    for (int i = 0; i < Exit.size(); i++) {
 			System.out.println(i+ "- "+ Exit.get(i));
 		}
 	    for (int i = 0; i < Exit.size(); i++) {
 			System.out.println(i+ "- "+Exitt.get(i));
 		}
+	    //String t=("Peter should buy books whose prices are "+sumados[0]+ "and"+sumados[1]);
+	    //String tr=("Peter should buy books whose prices are "+sumados1[0]+ "and"+sumados1[1]);
+	    //System.out.println(t);
+	    //System.out.println(tr);
 	}
-	public int BinarySearch1(int arr[], int x) {
-	   int lo = 0, hi = arr.length - 1;
-	   while (lo <= hi) {
-		   int mid = lo + (hi - lo) / 2;
-
-	       if (arr[mid] == x)
-	    	   return mid;
-
-	       if (arr[mid] < x)
-	           lo = mid + 1;
-
-	       else
-	           hi = mid - 1;
-	       }
-	       return -1;
-	    }
+	
+	
+	public int[] busqueda(ArrayList<int[]> num) {
+		int[] sum1 = new int[num.size()];
+		for(int i=0;i<num.size();i++) {
+			int[] n1=num.get(i);
+			int sum=n1[1]-n1[0];
+			sum1[i]=sum;
+		}
+		System.out.println(Arrays.toString(sum1));
+		int mayor, menor;
+        mayor = menor = sum1 [0];
+       
+        for (int i = 0; i < sum1.length; i++) {
+            if(sum1 [i] > mayor) {
+                mayor = sum1[i];
+            }
+            if(sum1[i]<menor) {
+                menor = sum1[i];
+            }
+        }
+        int[] nu2=new int[2];
+        for (int i = 0; i < sum1.length; i++) {
+        	int x= sum1[i]+menor;
+        	int validation= BinarySearch1( sum1, x);
+        	if(validation !=-1 && validation !=0) {
+        		nu2=num.get(i);
+        		System.out.println(Arrays.toString(nu2));
+        		return nu2;
+        	}
+        }
+		
+		return null;
+		
+	}
 	
 }
